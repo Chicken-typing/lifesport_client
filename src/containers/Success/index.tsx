f3beaed21e87d566429abe88ec39ea6236bb07b6
Lamvo27072001 <lampham27072001@gmail.com>
2023-12-28 15:33:44 +0700

    fix bug

--- src/containers/Success/index.tsx
+++ src/containers/Success/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { KaImage, Link } from '@components/primitive';
+import { Button, KaImage, Link } from '@components/primitive';
 import Background from '@svg/bg-home-new.svg';
+import { useQueryClient } from '@tanstack/react-query';
 
 function Success() {
+  const queryClient = useQueryClient();
+  const handleFetch = () => {
+    queryClient.invalidateQueries(['OrderTemp']);
+  };
   return (
     <div className="content-success">
       <Background />
-      <Link className="button" title="" href="/">
-        Back to Home
-      </Link>
+      <Button onClick={handleFetch}>
+        <Link className="button" title="" href="/">
+          Back to Home
+        </Link>
+      </Button>
     </div>
   );
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,35 +7,6 @@ import { createEmotionCache } from '@utils/createEmotionCache';
 import { useRouter } from 'next/router';
 import { ToastContainer } from 'react-toastify';
 const clientSideEmotionCache = createEmotionCache();
-import { useEffect, useState } from 'react';
-import LoadingScreen from '@components/compound/LoadingScreen';
-
-// export const Loading = () => {
-//   const router = useRouter();
-//   const [loading, setLoading] = useState<boolean>(false);
-//   const handleStart = (url: string) => url !== router.asPath && setLoading(true);
-//   const handleComplete = (url: string) =>
-//     url === router.asPath && setTimeout(() => setLoading(false));
-//   useEffect(() => {
-//     router.events.on('routeChangeStart', handleStart);
-//     router.events.on('routeChangeComplete', handleComplete);
-//     router.events.on('routeChangeError', handleComplete);
-
-//     return () => {
-//       router.events.off('routeChangeStart', handleStart);
-//       router.events.off('routeChangeComplete', handleComplete);
-//       router.events.off('routeChangeError', handleComplete);
-//     };
-//   });
-//   useEffect(() => {
-//     if (loading) {
-//       document.body.classList.add('no-scroll');
-//     } else {
-//       document.body.classList.remove('no-scroll');
-//     }
-//   }, [loading]);
-//   return loading ? <LoadingScreen /> : null;
-// };
 
 function App({ Component, emotionCache = clientSideEmotionCache, ...pageProps }: any) {
   const router = useRouter();
@@ -44,7 +15,6 @@ function App({ Component, emotionCache = clientSideEmotionCache, ...pageProps }:
     <CacheProvider value={emotionCache}>
       <LifeTravelProvider hydrateState={pageProps.dehydratedState}>
         <ToastContainer />
-        {/* <Loading></Loading> */}
         <Component {...pageProps} key={router.asPath} />
         <ReactQueryDevtools initialIsOpen={process.env.NODE_ENV === 'development'} />
       </LifeTravelProvider>

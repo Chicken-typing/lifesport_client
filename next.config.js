8ea1e3e2305d97458ebf27e52cf03c098ca31c50
Lamvo27072001 <lampham27072001@gmail.com>
2024-01-02 11:30:56 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,25 @@ const nextConfig = nextTranslate({
   productionBrowserSourceMaps: false,
   poweredByHeader: false,
 
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // replace this your actual origin
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.(png|jpg|jpeg|gif|eot|ttf|woff|woff2)$/,
